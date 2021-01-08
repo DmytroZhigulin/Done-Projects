bd49ad13d07d2575a994444221fefa2793930ef2
Dmytro <dmitryzhigulin87@gmail.com>
2021-01-08 16:44:56 +0200

    Changes test 3

--- Страховая/server.php
+++ Страховая/server.php
@@ -1,4 +1,4 @@
 <?php
-    // $_POST = json_decode(file_get_contents("php://input"), true); //нужно при работе с JSON => получение данных в JSON
+    $_POST = json_decode(file_get_contents("php://input"), true); //нужно при работе с JSON => получение данных в JSON
     echo var_dump($_POST);
 ?>    
