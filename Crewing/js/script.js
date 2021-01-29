eda7d708da672d1166580d34bdce8b42379a9555
Dmytro <dmitryzhigulin87@gmail.com>
2021-01-29 21:10:20 +0200

    Test 3

--- Crewing/js/script.js
+++ Crewing/js/script.js
@@ -25,7 +25,45 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
     formValidate();
+
+
+    let nav = document.querySelectorAll('.nav-item');
+    console.log(nav);
+    let sections = document.querySelectorAll('section');
+    console.log(sections);
+
+    //Функция принимает аргумент i, перебирает секции и присваевает им классы
+    //для скрытия, потом для секции с индексом который передается в i присваевает класс для показа.
+    function showDefaultWindow(i) {
+        sections.forEach( item => {
+            item.classList.add('hidden');
+            item.classList.remove('active');
+            console.log('done');
+        });
+        sections[i].classList.add('active');
+        sections[i].classList.remove('hidden');
+    }
+    showDefaultWindow(0);
+    
+    //Функция отслеживает клик по ссылкам меню и перевает индекс
+    // элемента события i + 1 в функцию выше.
+    function showPages() {
+         nav.forEach( (item, index) => {
+            
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+                showDefaultWindow(index);
+            });
+         });
+
+         //Add animation on logo link.
+         let logo = document.querySelector('.logo');
+         logo.addEventListener('click', () => showDefaultWindow(0));
+    }
+    showPages();
+
 }, false);
+
 
 
     
